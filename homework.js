9416d85224f67a7b790e53550117e1d2fcd2c1fd
tatulikk <tatulikmumladze@gmail.com>
2023-09-09 15:56:48 +0400

    avtvirte homework

---
+++ homework.js
@@ -0,0 +1,35 @@
+//SAVARJISHO 8
+let num=5
+if(num>0){
+    console.log(num/5+(2+num)**3) 
+}
+else{
+    console.log((5*num+(3+num**5)/4))
+}
+
+
+
+//SAVARJISHO 10
+
+let num1 = 345345
+console.log(num1 % 10)
+
+
+
+
+
+
+// SAVARJISHO 11
+let year = 2024
+if(year % 400 == 0){
+    console.log("nakiani weliwadi")
+}
+else if(year % 100 ==0){
+    console.log("ar aris nakiani weliwadi")
+}
+else if(year % 4 == 0){
+    console.log("nakiani weliwadi")
+}
+else{
+    console.log("ar aris nakiani weliwadi")
+}
